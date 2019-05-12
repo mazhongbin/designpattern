5cc8e7bf4e121fb929307f00cf53bf62d61c3c79
tracy05 <364814714@qq.com>
2019-05-12 11:50:37 +0800

    Singleton

---
+++ DP01Singleton/V2/Singleton.cs
@@ -0,0 +1,27 @@
+﻿namespace DP01Singleton.V2
+{
+    public sealed class Singleton
+    {
+        static Singleton instance = null;
+
+        private static readonly object padlock = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ XUnitTestDP/DP01SingletonTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestDP
+{
+    public class DP01SingletonTest
+    {
+        [Fact]
+        public void SingletonV1_SingleThread()
+        {
+            DP01Singleton.V1.Singleton singleton1 = DP01Singleton.V1.Singleton.GetInstance;
+            DP01Singleton.V1.Singleton singleton2 = DP01Singleton.V1.Singleton.GetInstance;
+
+            Assert.Same(singleton1, singleton2);
+        }
+
+        [Fact]
+        public void SingletonV1_MultiThread()
+        {
+            DP01Singleton.V1.Singleton singleton1 = null;
+            DP01Singleton.V1.Singleton singleton2 = null;
+            Task.Run(() =>
+            {
+                singleton1 = DP01Singleton.V1.Singleton.GetInstance;
+                singleton2 = DP01Singleton.V1.Singleton.GetInstance;
+
+            });
+
+            Assert.Same(singleton1, singleton2);
+        }
+    }
+}
