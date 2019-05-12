280941e41b149e8df58203861cdcab1a9267d01d
tracy05 <364814714@qq.com>
2019-05-12 11:25:31 +0800

    Singleton

---
+++ DP01Singleton/V1/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace DP01Singleton.V1
+{
+    public sealed class Singleton
+    {
+        static Singleton instance = null;
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+    }
+}
---
+++ XUnitTestDP/DP01Singleton/SingletonTest.cs
@@ -0,0 +1,17 @@
+﻿using DP01Singleton.V1;
+using Xunit;
+
+namespace XUnitTestDP.DP01Singleton
+{
+    public class SingletonTest
+    {
+        [Fact]
+        public void SingletonV1()
+        {
+            Singleton singleton1 = Singleton.GetInstance;
+            Singleton singleton2 = Singleton.GetInstance;
+
+            Assert.Same(singleton1, singleton2);
+        }
+    }
+}
