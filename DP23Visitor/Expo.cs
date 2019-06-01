bdbe6c2a39c5561999956b8842cb0f55b6885488
tracy05 <364814714@qq.com>
2019-06-01 13:55:18 +0800

    visitor

---
+++ DP23Visitor/Expo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DP23Visitor
+{
+    public class Expo
+    {
+        private List<Museum> museums = new List<Museum>();
+
+        public Expo()
+        {
+            museums.Add(new ChinaMuseum());
+            museums.Add(new EnglandMuseum());
+        }
+
+        public void WelCome(Visitor visitor)
+        {
+            foreach (var item in museums)
+            {
+                item.Accept(visitor);
+            }
+        }
+    }
+}
