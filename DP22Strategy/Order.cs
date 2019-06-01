84100aed4acf1447b3518c7d64119f27b9c2c432
tracy05 <364814714@qq.com>
2019-06-01 10:55:43 +0800

    Strategy

---
+++ DP22Strategy/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace DP22Strategy
+{
+    public class Order
+    {
+        private double _total;
+        public Order(double total)
+        {
+            _total = total;
+        }
+
+        public void UsePromote(Strategy strategy)
+        {
+            strategy.Algorith(ref _total);
+        }
+    }
+}
